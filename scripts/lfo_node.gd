64fb4a086a49e4a460c9ca619c8f1031131efc6c
Philipp Keese <philipp.keese@student.hpi.de>
2023-05-09 16:40:15 +0200

    monitor connections on lfo node

--- scripts/lfo_node.gd
+++ scripts/lfo_node.gd
@@ -1,6 +1,21 @@
 extends BaseNode
 
 
+func _ready() -> void:
+	super._ready()
+	self.connection_created.connect(self._on_connection)
+	self.connection_destroyed.connect(self._on_disconnection)
+
+
 func run(sample_count: int) -> void:
-	# Consume all inputs to avoid memory leaks
 	pass
+
+
+func _on_connection(connection: Connection) -> void:
+	if connection.is_consumer(self):
+		$Debug.modulate = Color.RED
+
+
+func _on_disconnection(connection: Connection) -> void:
+	if connection.is_consumer(self):
+		$Debug.modulate = Color.GREEN
