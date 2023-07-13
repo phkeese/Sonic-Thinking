aff52b0177f831cc4a134a275ca292829e02c24c
Philipp Keese <philipp.keese@greyld.com>
2023-07-13 18:44:00 +0200

    Read frequency from input in signal generator

--- scripts/nodes/NASignalGeneratorNode.cs
+++ scripts/nodes/NASignalGeneratorNode.cs
@@ -3,6 +3,7 @@ using Godot;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 using SonicThinking.scripts.autoload;
+using SonicThinking.scripts.helpers;
 using SonicThinking.scripts.sample_providers;
 
 namespace SonicThinking.scripts.nodes;
@@ -12,56 +13,13 @@ public partial class NASignalGeneratorNode : NANode
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_frequency = GetNode<SpinBox>("Frequency/SpinBox");
-		_gainSlider = GetNode<HSlider>("GainSlider");
-		_typeButton = GetNode<OptionButton>("WaveForm/OptionButton");
-		_invertRight = GetNode<CheckBox>("InvertRight");
-
-		_frequency.ValueChanged += OnFrequencyChanged;
-		_gainSlider.ValueChanged += OnGainChanged;
-		_typeButton.ItemSelected += index => { _generator.Type = ResolveWaveType((int)index); };
-		_invertRight.Toggled += pressed => _generator.PhaseReverse[0] = pressed; 
-		
-		_generator.Gain = _gainSlider.Value;
-		_generator.Frequency = _frequency.Value;
-		_generator.Type = ResolveWaveType(_typeButton.Selected);
-		_generator.PhaseReverse[0] = _invertRight.ButtonPressed;
-
 		InputChanged += OnInputChanged;
 	}
 
 	private void OnInputChanged(NANode sender, int slotIndex, ISampleProvider input)
 	{
 		if (slotIndex != FrequencySlot) throw new IndexOutOfRangeException("Invalid input slot.");
-		_frequencyInput = input;
-		_frequency.Editable = input == null;
-	}
-
-	private SignalGeneratorType ResolveWaveType(int selected)
-	{
-		return (SignalGeneratorType)selected;
-	}
-
-	private void OnGainChanged(double value)
-	{
-		_generator.Gain = value;
-	}
-
-	private void OnFrequencyChanged(double value)
-	{
-		_generator.Frequency = value;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta)
-	{
-		if (_frequencyInput != null)
-		{
-			float[] f = new float[1];
-			_frequencyInput.Read(f, 0, 1);
-			_generator.Frequency = f[0];
-			_frequency.Value = f[0];
-		}
+		_frequencyProvider.Source = input;
 	}
 
 	protected override ISampleProvider GetOutput(int port)
@@ -69,14 +27,13 @@ public partial class NASignalGeneratorNode : NANode
 		return _generator;
 	}
 
-	public const int OutputSlot = 1;
 	private const int FrequencySlot = 0;
 
-	private readonly SignalGenerator _generator = new SignalGenerator(DefaultSampleRate, DefaultChannelCount);
+	private readonly RebindingProvider _frequencyProvider = new RebindingProvider();
+	private readonly LUTSignalGenerator _generator;
 
-	private ISampleProvider _frequencyInput;
-	private SpinBox _frequency;
-	private HSlider _gainSlider;
-	private OptionButton _typeButton;
-	private CheckBox _invertRight;
+	public NASignalGeneratorNode()
+	{
+		_generator = new LUTSignalGenerator(_frequencyProvider);
+	}
 }
---
+++ scripts/sample_providers/LUTSignalGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NAudio.Wave;
+using SonicThinking.scripts.nodes;
+
+namespace SonicThinking.scripts.sample_providers;
+
+class LUTSignalGenerator : ISampleProvider
+{
+    private readonly double[] _waveTable;
+    private double _phase;
+    private readonly ISampleProvider _frequencySource;
+
+    public LUTSignalGenerator(ISampleProvider frequencySource)
+    {
+        if (!Equals(frequencySource.WaveFormat, NANode.DefaultWaveFormat))
+        {
+            throw new ArgumentException("Frequency provider must have single channel, project sample rate and float encoding.");
+        }
+        
+        int sampleRate = WaveFormat.SampleRate;
+        
+        _waveTable = new double[sampleRate];
+        for (int index = 0; index < sampleRate; ++index)
+            _waveTable[index] = (float)Math.Sin(2 * Math.PI * (double)index / sampleRate);
+        // For sawtooth instead of sine: waveTable[index] = (float)index / sampleRate;
+
+        _frequencySource = frequencySource;
+    }
+
+    public WaveFormat WaveFormat => NANode.DefaultWaveFormat;
+
+    public int Read(float[] buffer, int offset, int count)
+    {
+        // Read next n frequency samples
+        float[] frequencies = ReadFrequency(buffer.Length, offset, count);
+        
+        for (int n = 0; n < count; ++n)
+        {
+            float frequency = frequencies[n + offset];
+            float phaseStep = _waveTable.Length * (frequency / WaveFormat.SampleRate);
+            
+            int waveTableIndex = (int)_phase % _waveTable.Length;
+            buffer[n + offset] = (float)(this._waveTable[waveTableIndex]);
+            _phase += phaseStep;
+            if (_phase > (double)_waveTable.Length)
+                _phase -= (double)_waveTable.Length;
+        }
+        return count;
+    }
+
+    private float[] ReadFrequency(int bufferSize, int offset, int count)
+    {
+        var buffer = new float[bufferSize];
+        _frequencySource.Read(buffer, offset, count);
+        return buffer;
+    }
+}
